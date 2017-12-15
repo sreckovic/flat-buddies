a51d134765576b823a908fbfdb38fdee673ffd24
Nemanja <nemanja.sreckovic@gmail.com>
2017-12-16 00:20:30 +0100

    Header
    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,38 +6,40 @@ import "./Header.css";
 const header = () => {
   return (
     <header className="header">
-      <h1>
-        <Link to={"/"}>FlatBuddies.co</Link>
-      </h1>
-      <h2>Share accommodation website.</h2>
+      <div className="container">
+        <h1>
+          <Link to={"/"}>FlatBuddies.co</Link>
+        </h1>
+        <h2>Share accommodation website.</h2>
 
-      <nav className="navbar" aria-label="main navigation">
-        <div className="navbar-menu is-active">
-          <ul>
-            <li>
-              <NavLink
-                to="/rooms/"
-                exact
-                // activeClassName="my-active"
-                // activeStyle={{ color: "#fa923f", textDecoration: "underline" }}
-              >
-                Rooms
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={{
-                  pathname: "/add-listing"
-                  //hash: "#submit",
-                  //search: "?quick-submit=true"
-                }}
-              >
-                List my place
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <nav className="navbar" aria-label="main navigation">
+          <div className="navbar-menu is-active">
+            <ul>
+              <li>
+                <NavLink
+                  to="/rooms/"
+                  exact
+                  // activeClassName="my-active"
+                  // activeStyle={{ color: "#fa923f", textDecoration: "underline" }}
+                >
+                  Rooms
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/add-listing"
+                    //hash: "#submit",
+                    //search: "?quick-submit=true"
+                  }}
+                >
+                  List my place
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
