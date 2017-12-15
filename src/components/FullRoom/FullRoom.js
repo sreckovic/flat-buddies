fae5df23329e12bd4c3bfc2fc61bad0b3cd2b7ec
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-12-15 17:32:56 +0100

    Rooms
    Rooms

---
+++ src/components/FullRoom/FullRoom.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const fullRoom = props => {
+  return (
+    {props.room.available ? (
+      <p>Available from {props.room.available}</p>
+    ) : null}
+  )
+};
+
+export default fullRoom;
--- src/components/SingleRoom/SingleRoom.js
+++ src/components/SingleRoom/SingleRoom.js
@@ -4,28 +4,52 @@ import { Link } from "react-router-dom";
 
 import "./SingleRoom.css";
 
-const flat = props => {
-  return (
-    <div className="room">
-      <Link to={"/rooms/" + props.room.id}>
-        <h3>
-          {/*{props.room.suburb}, {props.room.city}*/}
-          {props.room.address}
-        </h3>
+const singleRoom = props => {
+  let address, type, desc;
 
-        <p>Available from {props.room.available}</p>
-        <p>
-          {props.room.type} with {props.room.bathrooms}
-        </p>
-        <p>
-          {props.room.furnishing}
-          {props.room.length_of_stay}
-          {props.room.gender}
-        </p>
-        <p>{props.room.description}</p>
+  if (props.room.address) {
+    address = (
+      <Link to={"/rooms/" + props.room.id}>
+        <h3>{props.room.address}</h3>
       </Link>
+    );
+  } else {
+    address = null;
+  }
+
+  if (props.room.type && props.room.bathrooms) {
+    type = (
+      <p className="desc">
+        {props.room.type} with {props.room.bathrooms}
+      </p>
+    );
+  } else {
+    type = null;
+  }
+
+  if (props.room.description) {
+    desc = (
+      <p className="abstract">
+        {props.room.description.substring(0, 120) + "..."}{" "}
+      </p>
+    );
+  } else {
+    desc = null;
+  }
+
+  return (
+    <div className="singleRoom">
+      {address}
+      {type}
+      {desc}
+
+      <p>
+        {props.room.furnishing}
+        {props.room.length_of_stay}
+        {props.room.gender}
+      </p>
     </div>
   );
 };
 
-export default flat;
+export default singleRoom;
